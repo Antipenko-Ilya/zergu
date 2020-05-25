1269b2a9d362e02c97eb43a19ee1c0f8410dc47e
noragami <ladasfeed@mail.ru>
2020-05-25 16:50:33 +0300

    Контакты, модалка,шрифт, бг

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 function hideModal(){
-    let modal = document.querySelector('.modal');
+    console.log('ge')
+    let modal = document.querySelector('.modal_wrapper');
     // let body = document.querySelector('body');
-    if (modal.style.display==='flex') {
-        modal.style.display = 'none';
-        // body.addEventListener('click', hideModal());
+    if (event.target.className=='modal_wrapper') {
+        
+        modal.style.opacity = '0';
+        modal.style.transform = "translateY(100px)"
+        setTimeout(()=>{
+            modal.style.display = 'none';
+        }, 300)
+        document.removeEventListener('click', hideModal);
     }
 }
 function showModal(){
-    let modal = document.querySelector('.modal');
+    let modal = document.querySelector('.modal_wrapper');
     // let body = document.querySelector('body');
     if (modal.style.display==='none') {
         modal.style.display = 'flex';
-        // body.addEventListener('click', hideModal());
+        setTimeout(()=> {
+            modal.style.opacity = '1';
+            modal.style.transform = "translateY(0px)"
+        })
+        document.addEventListener('click', hideModal);
     }
 }
