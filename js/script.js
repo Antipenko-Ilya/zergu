9d88b9cfde4ece2f7cc97cde7eb9733d9b79d285
noragami <ladasfeed@mail.ru>
2020-05-27 14:22:24 +0300

    slider fixes

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
 
-function plusSliders(n) {
-    showSlides(slideIndex+=n);
-}
+let slides = document.querySelector('.slides').style.display= 'block';
 
 function showSlides(n){
-    let slides = document.querySelectorAll('.slides');
+    let slides = Array.from(document.querySelectorAll('.slides'));
 
-    console.log(slides);
-    
+    slides[slideIndex].style.opacity = '0';
+    let tempIndex = slideIndex;
+    setTimeout(()=>{
+            slides[tempIndex].style.display = 'none';
+            
+    }, 300)
 
-    if(n > slides.length){
+    slideIndex+=n;
+
+    if(slideIndex >= slides.length){
         slideIndex = 1;
     }
-    else if(n < 1) {
+    else if(slideIndex < 1) {
         slideIndex = slides.length;
     }
-    for(let i = 0; i < slides.length; i++) {
-        slides[i].style.display ='none';
-        console.log(slides[i].style.display);
-        
-    }
-    slides[slideIndex - 1].style.display ='block';
+    
+    setTimeout(()=>{
+        slides[slideIndex].style.display ='block';
+        setTimeout(()=>slides[slideIndex].style.opacity ='1')
+    }, 300)
 }
 
 function hideModal(){
