5699922b39f414a72c41999c4a710f320d3f0515
noragami <ladasfeed@mail.ru>
2020-06-01 23:58:05 +0300

    sendMail

--- js/script.js
+++ js/script.js
@@ -145,3 +145,49 @@ function setAgeData(is18) {
 
 //block scrolling
 $('body').addClass('stop-scrolling')
+
+
+//sendMail
+// function send(event, php){
+//     console.log("Отправка запроса");
+//     event.preventDefault ? event.preventDefault() : event.returnValue = false;
+//     var req = new XMLHttpRequest();
+//     req.open('POST', php, true);
+//     req.send(new FormData(event.target));
+//     // req.onload = function() {
+//     //     if (req.status >= 200 && req.status < 400) {
+//     //     json = JSON.parse(this.response); // Ебанный internet explorer 11
+//     //         console.log(json);
+            
+//     //         // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+//     //         if (json.result == "success") {
+//     //             // Если сообщение отправлено
+//     //             alert("Сообщение отправлено");
+//     //         } else {
+//     //             // Если произошла ошибка
+//     //             alert("Ошибка. Сообщение не отправлено");
+//     //         }
+//     //     // Если не удалось связаться с php файлом
+//     //     } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
+    
+//     // // Если не удалось отправить запрос. Стоит блок на хостинге
+//     // req.onerror = function() {alert("Ошибка отправки запроса");};
+    
+//     }
+
+function call() {
+    console.log(123)
+    var msg   = $('#forms').serialize();
+      $.ajax({
+        type: 'POST',
+        url: '../mail.php', // Если обработчик лежит не рядом с формой, то здесь следует указать полный путь до него. Например http://example.com/mail.php
+        data: msg,
+        success: function(data) {
+          $('#results').html(data);
+        },
+        error:  function(xhr, str){
+              alert('Возникла ошибка: ' + xhr.responseCode);
+          }
+      });
+
+  }
